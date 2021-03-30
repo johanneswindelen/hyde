9b641755f4010099248e09d04f1289e84624c0bd
Johannes Windelen <mail@johanneswindelen.com>
2021-03-29 18:49:56 -0700

    add html rendering of indices and sites, tests

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,34 +1,44 @@
 from pathlib import Path
 import datetime
+from bs4 import BeautifulSoup
 
 import unittest
 
-from hyde.pages import Metadata, Page
-from .utils import page_from_file_str, TEST_PAGE_FILES
+from hyde.pages import Metadata, ContentPage
+from .utils import *
 
 
-class TestPage(unittest.TestCase):
+class TestContentPage(unittest.TestCase):
     def setUp(self) -> None:
         self.m_post = Metadata("test title", "posts", "test-title-stub")
 
-    def test_Page_init(self):
-        p = Page(self.m_post, None)
+    def test_page_init(self):
+        p = ContentPage(self.m_post, None)
         self.assertEqual(p.content, None)
         self.assertEqual(p.template_file, "posts.html.jinja2")
-        self.assertEqual(p.url, "test-title-stub.html")
+        self.assertEqual(p.url, "/test-title-stub.html")
 
-    def test_Page_from_file(self):
+    def test_page_from_file(self):
         test_file = TEST_PAGE_FILES[0]
         p = page_from_file_str(test_file)
 
         self.assertEqual(p.meta.title, "Test post")
-        self.assertEqual(p.meta.type, "posts")
+        self.assertEqual(p.meta.template, "posts")
         self.assertEqual(p.meta.date, datetime.date(year=2021, month=3, day=1))
         self.assertEqual(p.meta.author, "Hyde")
         self.assertEqual(p.meta.urlstub, "test-title-stub")
         self.assertEqual(p.content, test_file["html"])
         self.assertEqual(p.template_file, "posts.html.jinja2")
-        self.assertEqual(p.url, "test-title-stub.html")
+        self.assertEqual(p.url, "/test-title-stub.html")
+
+    def test_page_render_has_navbar(self):
+        test_file = TEST_PAGE_FILES[0]
+        p = page_from_file_str(test_file)
+
+        html_doc = p.render(get_jinja2_env(), nav_bar_pages=nav_bar_pages)
+        soup = BeautifulSoup(html_doc, features="html.parser")
+
+        assert_expected_hrefs_in_soup(soup, ["/index.html"])        
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_paginator.py
+++ tests/test_paginator.py
@@ -4,11 +4,14 @@ from bs4 import BeautifulSoup
 
 from hyde.pages import Page
 from hyde.paginator import Paginator
-from .utils import page_from_file_str, TEST_PAGE_FILES, get_jinja2_env
+from .utils import *
 
 class PaginatorTests(unittest.TestCase):
     def setUp(self):
         self.pages = [page_from_file_str(test_file) for test_file in TEST_PAGE_FILES]
+    
+    def tearDown(self):
+        del self.pages
         
     def test_paginator_with_pages(self):      
         paginator = Paginator(name="posts", content=self.pages, items_per_page=2)
@@ -17,15 +20,15 @@ class PaginatorTests(unittest.TestCase):
         index = next(paginator)
         self.assertEqual(len(index.items), 2)
         self.assertEqual(index.items[0].meta.title, "Test post")
-        self.assertEqual(index.items[0].url, "test-title-stub.html")
+        self.assertEqual(index.items[0].url, "/posts/test-title-stub.html")
         self.assertEqual(index.number, 0)
-        self.assertEqual(index.url, "posts/index.html")
+        self.assertEqual(index.url, "/posts/index.html")
         index = next(paginator)
         self.assertEqual(len(index.items), 1)
         self.assertEqual(index.items[0].meta.title, "Test post 3")
-        self.assertEqual(index.items[0].url, "test-post-3.html")
+        self.assertEqual(index.items[0].url, "/posts/test-post-3.html")
         self.assertEqual(index.number, 1)
-        self.assertEqual(index.url, "posts/index2.html")
+        self.assertEqual(index.url, "/posts/index2.html")
 
     def test_paginator_length_items_smaller_items_per_page(self):
         paginator = Paginator(name="posts", content=self.pages, items_per_page=10)
@@ -48,36 +51,30 @@ class PaginatorTests(unittest.TestCase):
         self.assertEqual(paginator.has_prev, True)
 
     def souped_index(self, paginator):
-        jinja2env = get_jinja2_env()
-
         index = next(paginator)
-        html_doc = index.render(jinja2env, paginator)
+        html_doc = index.render(get_jinja2_env(), paginator, nav_bar_pages=nav_bar_pages)
         return BeautifulSoup(html_doc, features="html.parser")
-
-    def assert_expected_href_in_soup(self, soup, expected):
-        links = [a.get("href") for a in soup.find_all('a')]
-        for expected_href in expected:
-            self.assertIn(expected_href, links)
-
-    def assert_expected_a_text_in_soup(self, soup, expected):
-        titles = [a.string for a in soup.find_all('a')]
-        for expected_title in expected:
-            self.assertIn(expected_title, titles)
 
     def test_paginator_render_contains_links(self):
         paginator = Paginator(name="posts", content=self.pages, items_per_page=10)
         soup = self.souped_index(paginator)
 
-        self.assert_expected_href_in_soup(soup, ["posts/test-title-stub.html", "posts/test-post-2.html", "posts/test-post-3.html"])
-        self.assert_expected_a_text_in_soup(soup, ["Test post", "Test post 2", "Test post 3"])
+        assert_expected_hrefs_in_soup(soup, ["/posts/test-title-stub.html", "/posts/test-post-2.html", "/posts/test-post-3.html"])
+        assert_expected_a_texts_in_soup(soup, ["Test post", "Test post 2", "Test post 3"])
 
     def test_paginator_render_has_prev_next_links(self):
         paginator = Paginator(name="posts", content=self.pages, items_per_page=1)
         soup = self.souped_index(paginator)
-        self.assert_expected_href_in_soup(soup, ["posts/index2.html"])
+        assert_expected_hrefs_in_soup(soup, ["/posts/index2.html"])
 
         soup = self.souped_index(paginator)
-        self.assert_expected_href_in_soup(soup, ["posts/index.html", "posts/index3.html"])
+        assert_expected_hrefs_in_soup(soup, ["/posts/index.html", "/posts/index3.html"])
 
         soup = self.souped_index(paginator)
-        self.assert_expected_href_in_soup(soup, ["posts/index2.html"])
+        assert_expected_hrefs_in_soup(soup, ["/posts/index2.html"])
+
+    def test_paginator_render_has_nav_bar(self):
+        paginator = Paginator(name="posts", content=self.pages, items_per_page=1)
+        soup = self.souped_index(paginator)
+
+        assert_expected_hrefs_in_soup(soup, ["/index.html"])
